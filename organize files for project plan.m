4efedb059f1d91a60ba090d462e07ce2f6dda1d1
gblackhorn <guoda.gd@gmail.com>
2023-03-01 17:47:25 +0900

    Add the plan (persudo code)

---
+++ organize files for project plan.m
@@ -0,0 +1,28 @@
+% Create a folder containing various subfolders for a project
+	% analysis
+	% presentations
+		% poster
+		% talk
+	% publications
+	% data: create links to the real locations
+	% ......
+	% unknow category
+	
+
+
+% input a folder type (for example: presentation/poster), search in a specific
+% folder (where I store all different presentations) and its subfolders with
+% GUI by using some keywords (IO, inferior olive)
+
+
+
+% Generate a list of folders and files would be organized
+% create a list containing the original and new locations of files
+
+
+
+% Confirm and organize the files if date strings are found in folders and file
+% names, organize them using the date information (yyyymm)
+
+
+% Check duplications
